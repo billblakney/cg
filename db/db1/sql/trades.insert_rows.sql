b73bbd3827f88a315f2c3fdfd03fb22b2fd17faf
billblakney <b.blakney@cox.net>
2016-06-08 09:45:09 -0400

    separate out insert trades

---
+++ db/db1/sql/trades.insert_rows.sql
@@ -0,0 +1,10 @@
+INSERT INTO trade VALUES ('Main(ET)',1,'2000-04-10','Buy','NEOP',3000,1.0,19.95,null);
+INSERT INTO trade VALUES ('Main(ET)',2,'2000-04-20','Buy','NEOP',4000,1.5,19.95,null);
+INSERT INTO trade VALUES ('Main(ET)',3,'2001-04-15','Sell','NEOP',5000,2.0,19.95,null);
+INSERT INTO trade VALUES ('Main(ET)',4,'2001-04-30','Sell','NEOP',2000,3.0,19.95,null);
+INSERT INTO trade VALUES ('Dad',1,'2010-04-10','Buy','IFLO',3000,1.0,19.95,null);
+INSERT INTO trade VALUES ('Dad',2,'2010-04-20','Buy','IFLO',4000,1.5,19.95,null);
+INSERT INTO trade VALUES ('Dad',3,'2011-04-15','Sell','IFLO',5000,2.0,19.95,null);
+INSERT INTO trade VALUES ('Dad',4,'2011-04-30','Sell','IFLO',2000,3.0,19.95,null);
+commit;
+
